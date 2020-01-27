ce2249790ada2ba329b34a0ac0f9e0e38c40d059
Gabriel Walsh <gabriel.walsh@moleskine.com>
2020-01-27 08:31:02 -0500

    initial setup

---
+++ msk-pocket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  msk-pocket
+//
+//  Created by Gabriel Walsh on 1/27/20.
+//  Copyright © 2020 Gabriel Walsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
